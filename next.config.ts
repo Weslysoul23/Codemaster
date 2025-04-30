39b488733d696e1abc2aa88c099ab329c690aecf
Weslysoul23 <mogurihirai9623@gmail.com>
2025-04-30 21:33:45 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '',
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
