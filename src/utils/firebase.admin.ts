3dad45684bb3b9bd598f64e0321d65dacd091747
Weslysoul23 <mogurihirai9623@gmail.com>
2025-11-10 03:18:29 +0800

    test

--- src/utils/firebase.admin.ts
+++ src/utils/firebase.admin.ts
@@ -1,21 +1,18 @@
 import admin, { ServiceAccount } from "firebase-admin";
 
 const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: privateKey as string,
 };
 
-export function getFirebaseAdmin() {
-  if (typeof window === 'undefined') {
-    if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-      });
-    }
-  }
-  return admin;
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.DATABASE_URL, // optional if using RTDB
+  });
 }
 
-export const firebaseAdmin = getFirebaseAdmin();
+export const firebaseAdmin = admin;
